c84d82ede26df208e6b1dfe9597aa2ee3dc6316d
Małgorzata Skrudlik <sophie.gosia@gmail.com>
2018-04-09 22:56:00 +0200

    Add style in css

--- js/script.js
+++ js/script.js
@@ -10,13 +10,13 @@ Button.prototype = {
 		this.$element.click(function() {
 			alert(self.text);
     });
-    this.$element.appendTo($('body'));
+    this.$element.appendTo($('.buttons'));
 		this.$element.addClass('button');
 	}
 };
 
 var btn1 = new Button('Hello!');
-var btn2 = new Button('Hola!');
+var btn2 = new Button('Ahoj!');
 var btn3 = new Button('Zdravo!');
 var btn4 = new Button('Cześć!');
 
